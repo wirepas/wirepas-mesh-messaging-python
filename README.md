726a43c541abc9816c8a06b53dd25c71ff5b95f3
Gwendal Raoul <gwendal.raoul@wirepas.com>
2020-12-01 10:48:08 +0100

    Initial commit:
    Files are moved from wirepas-messaging repo

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# wirepas-mesh-messaging-python
-Wheel to ease the interaction with Backend to Gateway API in python (protobuf)
+# Wirepas Mesh Messaging
+
+This wheel contains the generated code to interact with Wirepas Mesh Network
+ through the Gateway to Backend API.
+
+## Installation
+
+### Install from PyPi
+
+This package is available from [PyPi][pypi].
+
+```shell
+    pip install wirepas-mesh-messaging
+```
+
+### From the source
+
+
+## Services API documentation
+
+Please refer to the [backend apis repository][github_backend_apis] for
+documentation on the Wirepas' services APIs.
+
+
+## License
+
+Licensed under the Apache License, Version 2.0.
+See [LICENSE][here_license] for the full license text.
+
+[pypi]: https://pypi.org/project/wirepas-messaging/
+
+[github_backend_apis]: https://github.com/wirepas/backend-apis
---
+++ wirepas_mesh_messaging/otap_helper.py
@@ -0,0 +1,37 @@
+"""
+    Gateway result code
+    ===================
+
+    .. Copyright:
+        Copyright 2019 Wirepas Ltd under Apache License, Version 2.0.
+        See file LICENSE for full license details.
+"""
+
+# flake8: noqa
+from .proto.otap_message_pb2 import BLANK, PRESENT, PROCESS, SUCCESS, NEW, ERROR
+
+import enum
+
+
+class ScratchpadType(enum.Enum):
+    SCRATCHPAD_TYPE_BLANK = BLANK
+    SCRATCHPAD_TYPE_PRESENT = PRESENT
+    SCRATCHPAD_TYPE_PROCESS = PROCESS
+
+
+class ScratchpadStatus(enum.Enum):
+    SCRATCHPAD_STATUS_SUCCESS = SUCCESS
+    SCRATCHPAD_STATUS_NEW = NEW
+    SCRATCHPAD_STATUS_ERROR = ERROR
+
+
+def parse_scratchpad_info(message_obj, dic):
+    dic["len"] = message_obj.len
+    dic["crc"] = message_obj.crc
+    dic["seq"] = message_obj.seq
+
+
+def set_scratchpad_info(message_obj, dic):
+    message_obj.len = dic["len"]
+    message_obj.crc = dic["crc"]
+    message_obj.seq = dic["seq"]
