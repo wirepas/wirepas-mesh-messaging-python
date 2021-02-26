7fef5a8daeb7b96990792730c0b288656c63116b
Gwendal Raoul <gwendal.raoul@wirepas.com>
2021-02-26 11:17:41 +0100

    Api: support network address in rx event
    Add also support for new error code

--- wirepas_mesh_messaging/received_data.py
+++ wirepas_mesh_messaging/received_data.py
@@ -30,7 +30,8 @@ class ReceivedDataEvent(Event):
         data(bytearray): the received data or None if must be hidden
         data_size(int): the received data size, only needed if data is None
         event_id(int): event unique id (random value generated if None)
-        hop_count(int): number of hop for th emessage to reach the gateway
+        hop_count(int): number of hop for the message to reach the gateway
+        network_address(int): network_address of this message
     """
 
     def __init__(
@@ -48,6 +49,7 @@ class ReceivedDataEvent(Event):
         data_size=None,
         event_id=None,
         hop_count=0,
+        network_address=None,
         **kwargs
     ):
         super(ReceivedDataEvent, self).__init__(
@@ -68,6 +70,7 @@ class ReceivedDataEvent(Event):
 
         self.data_size = data_size
         self.hop_count = hop_count
+        self.network_address = network_address
 
     @classmethod
     def from_payload(cls, payload):
@@ -101,6 +104,13 @@ class ReceivedDataEvent(Event):
             # Attribute is not defined
             payload_size = None
 
+        # Check optional network_address field
+        if event.HasField("network_address"):
+            network_address = event.network_address
+        else:
+            # Network address is not set
+            network_address = None
+
         return cls(
             d["gw_id"],
             d["sink_id"],
@@ -115,6 +125,7 @@ class ReceivedDataEvent(Event):
             data_size=payload_size,
             event_id=d["event_id"],
             hop_count=hop_count,
+            network_address=network_address
         )
 
     @property
@@ -140,5 +151,8 @@ class ReceivedDataEvent(Event):
 
         if self.hop_count > 0:
             event.hop_count = self.hop_count
+
+        if self.network_address is not None:
+            event.network_address = self.network_address
 
         return message.SerializeToString()
