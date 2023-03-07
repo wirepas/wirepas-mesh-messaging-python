703a35144166b20fb9c08fc23c35d079d04f467f
Gwendal Raoul <gwendal.raoul@wirepas.com>
2023-03-07 15:54:25 +0100

    Extend gateway status
    Bump protobuff version and implement the extended status

--- wirepas_mesh_messaging/status.py
+++ wirepas_mesh_messaging/status.py
@@ -17,6 +17,14 @@ from .wirepas_exceptions import GatewayAPIParsingException
 # This API should never be changes in future (purpose of protobuf)
 PB_MESSAGE_DEFINITION_VERSION = 1
 
+from .config_helper import (
+    parse_config_rw,
+    parse_config_ro,
+    parse_config_otap,
+    set_config_ro,
+    set_config_rw,
+    set_config_otap
+)
 
 class GatewayState(enum.Enum):
     """
@@ -42,12 +50,28 @@ class StatusEvent(Event):
         state (GatewayState): state of the gateway
         version (int): protobuf messsage definition version for the gateway. This implementation can only parse protobuf message definitions in version1
         event_id(int): event unique id (random value generated if None)
+        sink_configs (list): list of dictionnary containing the sink configs
+        gateway_model (string): gateway model
+        gateway_version (string): gateway version
     """
 
-    def __init__(self, gw_id, state, version=PB_MESSAGE_DEFINITION_VERSION, event_id=None, **kwargs):
+    def __init__(
+        self,
+        gw_id,
+        state,
+        version=PB_MESSAGE_DEFINITION_VERSION,
+        event_id=None,
+        sink_configs=[],
+        gateway_model='',
+        gateway_version='',
+        **kwargs
+    ):
         super(StatusEvent, self).__init__(gw_id, event_id=event_id, **kwargs)
         self.state = state
         self.version = version
+        self.sink_configs = sink_configs
+        self.gateway_model = gateway_model
+        self.gateway_version = gateway_version
 
     @classmethod
     def from_payload(cls, payload):
@@ -69,8 +93,26 @@ class StatusEvent(Event):
         if event.version != PB_MESSAGE_DEFINITION_VERSION:
             raise RuntimeError("Unsupported gateway message definition version. The only supported version is " + str(PB_MESSAGE_DEFINITION_VERSION))
 
+        configs = []
+        for conf in event.configs:
+            config = {}
+
+            config["sink_id"] = conf.sink_id
+
+            parse_config_rw(conf, config)
+            parse_config_ro(conf, config)
+            parse_config_otap(conf, config)
+
+            configs.append(config)
+
         d = Event._parse_event_header(event.header)
-        return cls(d["gw_id"], online, event_id=d["event_id"], time_ms_epoch=d["time_ms_epoch"])
+        return cls(d["gw_id"],
+                   online,
+                   event_id=d["event_id"],
+                   sink_configs=configs,
+                   time_ms_epoch=d["time_ms_epoch"],
+                   gateway_model=event.gw_model,
+                   gateway_version=event.gw_version)
 
     @property
     def payload(self):
@@ -86,5 +128,20 @@ class StatusEvent(Event):
             status.state = ON
         else:
             status.state = OFF
+
+        if self.sink_configs is not None:
+            for config in self.sink_configs:
+                conf = status.configs.add()
+                conf.sink_id = config["sink_id"]
+
+                set_config_rw(conf, config)
+                set_config_ro(conf, config)
+                set_config_otap(conf, config)
+
+        if self.gateway_model is not None:
+            status.gw_model = self.gateway_model
+
+        if self.gateway_version is not None:
+            status.gw_version = self.gateway_version
 
         return message.SerializeToString()
