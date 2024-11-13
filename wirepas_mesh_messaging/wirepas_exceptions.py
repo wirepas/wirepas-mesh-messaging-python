096950ac0f222585a92f108d60ee088640c48147
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2024-11-13 18:00:01 +0200

    Improve errors for SetScratchpadTargetAndActionRequest.from_payload()
    If constructing a new SetScratchpadTargetAndActionRequest from payload
    fails due to invalid values, a new InvalidMessageContents exception is
    raised. This exception can hold the header of the request message, and
    it would be possible to generate a response with the corresponding
    req_id based on the exception.

--- wirepas_mesh_messaging/wirepas_exceptions.py
+++ wirepas_mesh_messaging/wirepas_exceptions.py
@@ -17,3 +17,16 @@ class InvalidMessageType(GatewayAPIParsingException):
     """
     Exception indicating wrong message type during deserialization
     """
+
+class InvalidMessageContents(GatewayAPIParsingException):
+    """
+    Exception indicating invalid message contents in the received message.
+
+    Parameters:
+        - message: The exception message
+        - header: Parsed header of the message, as a dictionary
+    """
+
+    def __init__(self, message, header):
+        super().__init__(message)
+        self.header = header
