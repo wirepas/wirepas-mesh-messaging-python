5d3e2f66c376814facdd919725e23c03993a6df4
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2024-12-13 17:51:26 +0200

    LG-75 Tests for CDD API
    Following classes are now verified to support the new CDD API:
    - SetConfigRequest
    - SetConfigResponse
    - GetConfigsResponse
    - StatusEvent
    
    Also refactoring the tests

--- tests/test_set_config.py
+++ tests/test_set_config.py
@@ -2,9 +2,9 @@
 
 import wirepas_mesh_messaging
 import pytest
-import base64
 from google.protobuf.json_format import ParseDict
 from wirepas_mesh_messaging.proto import GenericMessage
+from test_utils import get_cdc_with_base64
 from default_value import *
 
 
@@ -34,13 +34,17 @@ def test_generate_parse_response():
         assert v == request2.__dict__[k]
 
 def test_parse_request_with_config_data_items():
-    message_description = _get_request_base()
-    config = message_description["wirepas"]["set_config_req"]["config"]
-    config["configuration_data_content"] = [
-        { "endpoint": 10,      "payload": _hex_to_base64("AABBCC") },
-        { "endpoint": 0x10000, "payload": _hex_to_base64("112233EEFF") },
-        { "endpoint": 0,       "payload": _hex_to_base64("") }
-    ]
+    message_description = {
+        "wirepas": {
+            "set_config_req": {
+                "header": {"req_id": "1"},
+                "config": {
+                    "sink_id": "sink0",
+                    "configuration_data_content": get_cdc_with_base64(TEST_CDC1)
+                },
+            }
+        }
+    }
     message = ParseDict(message_description, GenericMessage())
     assert message.IsInitialized()
 
@@ -48,20 +52,20 @@ def test_parse_request_with_config_data_items():
         message.SerializeToString()
     )
 
-    parsed_cdc = request.new_config["configuration_data_content"]
-    assert len(parsed_cdc) == 3
-    for expected, parsed in zip(config["configuration_data_content"], parsed_cdc):
-        assert parsed["endpoint"] == expected["endpoint"]
-        assert parsed["payload"] == base64.b64decode(expected["payload"])
+    assert request.new_config["configuration_data_content"] == TEST_CDC1
 
 def test_parse_response_with_config_data_items():
-    message_description = _get_response_base()
-    config = message_description["wirepas"]["set_config_resp"]["config"]
-    config["configuration_data_content"] = [
-        { "endpoint": 30,      "payload": _hex_to_base64("CCBBAA") },
-        { "endpoint": 0x30000, "payload": _hex_to_base64("1122334455") },
-        { "endpoint": 0,       "payload": _hex_to_base64("") }
-    ]
+    message_description = {
+        "wirepas": {
+            "set_config_resp": {
+                "header": {"sink_id": "sink0", "req_id": "1", "gw_id": "1", "res": 0},
+                "config": {
+                    "sink_id": "sink0",
+                    "configuration_data_content": get_cdc_with_base64(TEST_CDC1)
+                },
+            }
+        }
+    }
     message = ParseDict(message_description, GenericMessage())
     assert message.IsInitialized()
 
@@ -69,54 +73,13 @@ def test_parse_response_with_config_data_items():
         message.SerializeToString()
     )
 
-    parsed_cdc = response.config["configuration_data_content"]
-    assert len(parsed_cdc) == 3
-    for expected, parsed in zip(config["configuration_data_content"], parsed_cdc):
-        assert parsed["endpoint"] == expected["endpoint"]
-        assert parsed["payload"] == base64.b64decode(expected["payload"])
-
-def test_create_request_with_config_data_items():
-    TEST_CONFIG = {
-        "sink_id": SINK_ID,
-        "configuration_data_content": [
-            { "endpoint": 5,       "payload": bytes.fromhex("1020304050") },
-            { "endpoint": 0x20000, "payload": bytes.fromhex("334455") },
-            { "endpoint": 30, "payload": bytes() }
-        ]
-    }
-    built_request = wirepas_mesh_messaging.SetConfigRequest(
-        SINK_ID, TEST_CONFIG, REQUEST_ID
-    )
-    parsed_request = wirepas_mesh_messaging.SetConfigRequest.from_payload(
-        built_request.payload
-    )
-
-    for k, v in built_request.__dict__.items():
-        assert v == parsed_request.__dict__[k]
-
-def test_create_response_with_config_data_items():
-    TEST_CONFIG = {
-        "sink_id": SINK_ID,
-        "configuration_data_content": [
-            { "endpoint": 5,       "payload": bytes.fromhex("1020304050") },
-            { "endpoint": 0x20000, "payload": bytes.fromhex("334455") },
-            { "endpoint": 30, "payload": bytes() }
-        ]
-    }
-    built_response = wirepas_mesh_messaging.SetConfigResponse(
-        REQUEST_ID, GATEWAY_ID, RES_OK, SINK_ID, TEST_CONFIG
-    )
-    parsed_response = wirepas_mesh_messaging.SetConfigResponse.from_payload(
-        built_response.payload
-    )
-
-    for k, v in built_response.__dict__.items():
-        assert v == parsed_response.__dict__[k]
+    assert response.config["configuration_data_content"] == TEST_CDC1
 
 INVALID_CDC_DATA = [
     ([{ "payload": bytes.fromhex("10") }], KeyError),
     ([{ "endpoint": 10 }], KeyError),
-    ({"endpoint": 10, "payload": bytes()}, TypeError)
+    ({"endpoint": 10, "payload": bytes()}, TypeError),
+    ([{"endpoint": -1, "payload": bytes([1,2,3])}], ValueError)
 ]
 @pytest.mark.parametrize("cdc,expected_error", INVALID_CDC_DATA)
 def test_request_with_invalid_configuration_data_content(cdc, expected_error):
@@ -138,25 +101,3 @@ def test_response_with_invalid_configuration_data_content(cdc, expected_error):
     with pytest.raises(expected_error):
         response.payload
 
-def _hex_to_base64(hex_str):
-    return base64.b64encode(bytes.fromhex(hex_str))
-
-def _get_request_base() -> dict:
-    return {
-        "wirepas": {
-            "set_config_req": {
-                "header": {"req_id": "1"},
-                "config": {"sink_id": "sink0"},
-            }
-        }
-    }
-
-def _get_response_base() -> dict:
-    return {
-        "wirepas": {
-            "set_config_resp": {
-                "header": {"sink_id": "sink0", "req_id": "1", "gw_id": "1", "res": 0},
-                "config": {"sink_id": "sink0"},
-            }
-        }
-    }
