8c22f6440c3ad401381bf52445eaadcb041715d0
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2025-02-19 17:50:16 +0200

    LG-75 Support for gateway feature flags
    Adds support for gateway feature flags in StatusEvent and GetGwInfoResp
    messages

--- tests/test_get_gw_info.py
+++ tests/test_get_gw_info.py
@@ -26,11 +26,32 @@ def test_generate_parse_response():
         "Version x.y",
         max_scratchpad_size=512,
         implemented_api_version=IMPLEMENTED_API_VERSION,
+        gateway_features=[
+            wirepas_mesh_messaging.GatewayFeature.GW_FEATURE_SCRATCHPAD_CHUNK_V1,
+            wirepas_mesh_messaging.GatewayFeature.GW_FEATURE_CONFIGURATION_DATA_V1
+        ]
     )
 
     request2 = wirepas_mesh_messaging.GetGatewayInfoResponse.from_payload(
         request.payload
     )
+
+    expected_members = [
+        "gw_id",
+        "sink_id",
+        "req_id",
+        "res",
+        "time_ms_epoch",
+        "current_time_s_epoch",
+        "gateway_model",
+        "gateway_version",
+        "implemented_api_version",
+        "max_scratchpad_size",
+        "gateway_features"
+    ]
+    for field in expected_members:
+        assert field in request.__dict__
+        assert field in request2.__dict__
 
     for k, v in request.__dict__.items():
         assert v == request2.__dict__[k]
@@ -52,5 +73,4 @@ def test_generate_parse_response_not_all_optional():
     )
 
     for k, v in request.__dict__.items():
-        print(k)
         assert v == request2.__dict__[k]
---
+++ wirepas_mesh_messaging/gateway_feature.py
@@ -0,0 +1,22 @@
+"""
+    Gateway feature
+    ==========
+
+    .. Copyright:
+        Copyright 2025 Wirepas Ltd under Apache License, Version 2.0.
+        See file LICENSE for full license details.
+"""
+
+import enum
+from .proto import GatewayFeature as GatewayFeature_pb
+
+class GatewayFeature(enum.Enum):
+    """
+    Class that represent all possible gateway feature flags.
+    Keep a one-to-one mapping with current protobuf flags to ease conversion.
+    """
+
+    GW_FEATURE_UNKNOWN = GatewayFeature_pb.UNKNOWN
+    GW_FEATURE_SCRATCHPAD_CHUNK_V1 = GatewayFeature_pb.SCRATCHPAD_CHUNK_V1
+    GW_FEATURE_CONFIGURATION_DATA_V1 = GatewayFeature_pb.CONFIGURATION_DATA_V1
+
