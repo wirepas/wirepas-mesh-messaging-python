352a5542fe64403d94ae9cef2f28538aa251e222
Şan Güneş <180301198+sgunes-wirepas@users.noreply.github.com>
2025-02-19 16:47:46 +0200

    LG-75 Add set_config_data_item
    This adds support for SetConfigurationDataItemReq and
    SetConfigurationDataItemResp messages for setting or removing a single
    item.

--- wirepas_mesh_messaging/__init__.py
+++ wirepas_mesh_messaging/__init__.py
@@ -32,6 +32,7 @@ from .set_scratchpad_target import SetScratchpadTargetAndActionRequest, SetScrat
 from .gateway_result_code import GatewayResultCode
 from .otap_helper import ScratchpadStatus, ScratchpadType, ScratchpadAction, ProcessingDelay
 from .wirepas_exceptions import GatewayAPIParsingException
+from .set_config_data_item import SetConfigurationDataItemRequest, SetConfigurationDataItemResponse
 
 from google.protobuf.internal import api_implementation
 
